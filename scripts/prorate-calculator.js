0092ebdb446bdcbb63896e6bc1b631e7b6e4112e
James <4589250+Naught0@users.noreply.github.com>
2019-11-18 17:18:52 -0500

    add stuff

---
+++ scripts/prorate-calculator.js
@@ -0,0 +1,4 @@
+function onClick() {
+    var startDate = new Date(document.getElementById("start-date"));
+    alert("Date input was: " + startDate);
+}
---
+++ scripts/show-menu.js
@@ -0,0 +1,4 @@
+function showMenu() {
+    document.getElementById("sidebar-menu").classList.toggle("show");
+    document.getElementById("menu").classList.toggle("is-active");
+}
