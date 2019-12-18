f397aebcdd4f242cc4ac0bd19adc9ebc01f699dc
James <4589250+Naught0@users.noreply.github.com>
2019-12-18 17:00:58 -0500

    add fun bits, change styles, mess w/ jquery
    yeet

---
+++ scripts/article-toggle.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.btn-icon').click(function() {
+        // Eventually this is going to look kinda like this 
+        // https://jsfiddle.net/pjQP7/10/
+        // Slide content left or right or something
+        if($(this).find($(".fa")).hasClass('fa')
+        $("i.fa-toggle-on").toggleClass('fa-rotate-180');
+    });
+});
