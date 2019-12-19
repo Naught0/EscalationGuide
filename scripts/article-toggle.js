7247ea44b79fe0e7b9fc2183cc5521ae5faa89d5
James <4589250+Naught0@users.noreply.github.com>
2019-12-19 18:19:58 -0500

    style changes & I figured out jquery

--- scripts/article-toggle.js
+++ scripts/article-toggle.js
@@ -1,9 +1,37 @@
 $(function() {
+    $(document).find('.mac-wrapper').hide();
+
     $('.btn-icon').click(function() {
-        // Eventually this is going to look kinda like this 
-        // https://jsfiddle.net/pjQP7/10/
-        // Slide content left or right or something
-        if($(this).find($(".fa")).hasClass('fa')
-        $("i.fa-toggle-on").toggleClass('fa-rotate-180');
+        // Toggle icon
+        var t = $(this).find('.is-toggle');
+
+        // Icons for win & mac
+        var winIcon = $(this).find('.fa-windows');
+        var macIcon = $(this).find('.fa-apple');
+
+        // Win vs mac content
+        var win = $(this).siblings('.windows-wrapper');
+        var mac = $(this).siblings('.mac-wrapper');
+
+        // Change to Mac
+        if (t.hasClass('fa-toggle-off')) {
+            t.addClass('fa-toggle-on');
+            t.removeClass('fa-toggle-off');
+            
+            win.fadeOut();
+            mac.fadeIn();
+            // win.addClass('hidden');
+            // mac.removeClass('hidden');
+        }
+        // Change to Windows
+        else {
+            t.addClass('fa-toggle-off');
+            t.removeClass('fa-toggle-on');
+
+            win.fadeIn();
+            mac.fadeOut();
+            // win.removeClass('hidden');
+            // mac.addClass('hidden');
+        }
     });
 });
