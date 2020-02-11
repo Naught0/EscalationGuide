4433b38c41433efa31ee618f3225ce6e425d45c5
James <4589250+Naught0@users.noreply.github.com>
2020-02-11 17:08:28 -0500

    wow that was easy

--- scripts/ts.js
+++ scripts/ts.js
@@ -1,18 +1,38 @@
 $(function () {
+
+    // Init clipboard 
+    var clip = new ClipboardJS('.cp-btn');
+
+    // Log success
+    clip.on('success', function(e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+    
+        e.clearSelection();
+    });
+
+    // Log fail
+    clip.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
+
+    // Notify copy on click
     $('#copy-catalina-fourty').click(function () {
-        var toCopy = document.getElementById('cmd-catalina-fourty').innerText;
-
-
-        var clip = new ClipboardJS('#copy-catalina-fourty', {
-            text: function () {
-                return toCopy;
-            }
-        })
-
-        $('#cmd-catalina-fourty').notify('Copied', {
+        $('#cmd-catalina-fourty').notify('Copied!', {
             className: 'success',
             position: 'right',
             autoHideDelay: 2000
-        });
+        })
+    })
+
+    // Notify copy on click
+    $('#btn-copy-catalina-kb').click(function() {
+        $('#cmd-catalina-kb').notify('Copied!', {
+            className: 'success',
+            position: 'right',
+            autoHideDelay: 2000
+        })
     })
 })
